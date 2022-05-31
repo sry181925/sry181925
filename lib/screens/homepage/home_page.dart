cc7a38c6ec428ff4e940676cdcca7a94b4200225
Saurabh Yadav <sry27091998@gmail.com>
2022-05-31 00:06:56 -0700

    name change

--- lib/screens/homepage/home_page.dart
+++ lib/screens/homepage/home_page.dart
@@ -65,7 +65,7 @@ Widget buildAdvertismentPlace(){
                 Container(
                   margin: EdgeInsets.all(6.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(10.0),
                     image: DecorationImage(
                       image: NetworkImage("https://cdn.acowebs.com/wp-content/uploads/2019/02/Impact-of-eCommerce-On-Society.png"),
                       fit: BoxFit.cover,
@@ -77,21 +77,20 @@ Widget buildAdvertismentPlace(){
                 Container(
                   margin: EdgeInsets.all(6.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(10.0),
                     image: DecorationImage(
                       image: NetworkImage(" https://cdn.dnaindia.com/sites/default/files/styles/full/public/2019/02/01/785054-ecommerce-istock-020119.jpg"),
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                  
                 //3rd Image of Slider
                 Container(
                   margin: EdgeInsets.all(6.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(10.0),
                     image: DecorationImage(
-                      image: NetworkImage("https://img.freepik.com/free-vector/e-commerce-icon-robotic-hand-internet-shopping-online-purchase-add-cart_127544-586.jpg?w=2000 "),
+                      image: NetworkImage('https://img.freepik.com/free-vector/e-commerce-icon-robotic-hand-internet-shopping-online-purchase-add-cart_127544-586.jpg?w=2000 '),
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -101,9 +100,9 @@ Widget buildAdvertismentPlace(){
                 Container(
                   margin: EdgeInsets.all(6.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(10.0),
                     image: DecorationImage(
-                      image: NetworkImage("https://www.hubspot.com/hubfs/ecommerce%20busines.jpg "),
+                      image: NetworkImage('https://www.hubspot.com/hubfs/ecommerce%20busines.jpg'),
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -113,7 +112,7 @@ Widget buildAdvertismentPlace(){
                 Container(
                   margin: EdgeInsets.all(6.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(10.0),
                     image: DecorationImage(
                       image: NetworkImage("https://www.itkhoj.com/wp-content/uploads/2017/08/E-Commerce-in-Hindi-678x381.png"),
                       fit: BoxFit.cover,
@@ -123,7 +122,7 @@ Widget buildAdvertismentPlace(){
   
           ],
               
-            //Slider Container properties
+//Slider Container properties
               options: CarouselOptions(
                 height: 180.0,
                 enlargeCenterPage: true,
