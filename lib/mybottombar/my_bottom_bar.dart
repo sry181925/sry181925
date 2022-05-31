385de761d71b80b414a9697ff553771e1d2735ae
Saurabh Yadav <sry27091998@gmail.com>
2022-05-31 02:34:23 -0700

    name change

--- lib/mybottombar/my_bottom_bar.dart
+++ lib/mybottombar/my_bottom_bar.dart
@@ -1,25 +1,36 @@
-import 'package:e_jkmm/appColors/app_colors.dart';
-//import 'package:e_jkmm/screens/category/category_screen.dart';
+import 'package:e_jkmm/appcolors/app_colors.dart';
 import 'package:e_jkmm/screens/homepage/home_page.dart';
-//import 'package:cheap_charly/screens/profilescreen/profile_screen.dart';
-//import 'package:cheap_charly/screens/wishList/wish_list_screen.dart';
-//import 'package:e_jkmm/screens/yourbag/your_bag_screen.dart';
-import 'package:custom_navigator/custom_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class MyBottomBar extends StatefulWidget {
+
+
   @override
   State<MyBottomBar> createState() => _MyBottomBarState();
 }
 
 class _MyBottomBarState extends State<MyBottomBar> {
   int _selectedIndex = 0;
-  static List<Widget>_widgetOptions = <Widget>[
-    HomePage(),
-    //CategoryScreen(),
-    //YourBagScreen(),
-    //WishListScreen(),
-    //ProfileScreen(),
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static  List<Widget> _widgetOptions = <Widget>[
+   HomePage(),
+    Text(
+      '2 page',
+      style: optionStyle,
+    ),
+    Text(
+      '3 page',
+      style: optionStyle,
+    ),
+    Text(
+      '4 paGE',
+      style: optionStyle,
+    ),
+    Text(
+      '5 PAGE',
+      style: optionStyle,
+    ),
   ];
 
   void _onItemTapped(int index) {
@@ -30,46 +41,45 @@ class _MyBottomBarState extends State<MyBottomBar> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      children: _widgetOptions,
-      onItemTap: _onItemTapped,
-      scaffold: Scaffold(
-        body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          type: BottomNavigationBarType.fixed,
-          unselectedItemColor: AppColors.baseGrey40Color,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.grid_view),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart_outlined),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_border),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: '',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedIconTheme: IconThemeData(
-            color: AppColors.baseDarkPinkColor,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: AppColors.baseGrey40Color,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+           
           ),
-          onTap: _onItemTapped,
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view),
+            label: '',
+          
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: ' ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: '',
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.baseDarkPinkColor,
+        onTap: _onItemTapped,
       ),
     );
   }
